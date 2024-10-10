380a9c516deb6615075f7d120ad15205622dcaa9
Liviu Ionescu <ilg@livius.net>
2024-10-10 21:37:46 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -41,9 +41,9 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from xPack Binary Development Tools`}
-      description="A binary package with the GNU texinfo executables">
+    <Layout 
+      title={`Hello from texinfo`}
+      description="A binary package with the texinfo executables">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
