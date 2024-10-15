e519f5dd121179cc71913d0a6c88bd9aafc44739
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:34:24 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to texinfo`}
+      title="Welcome!"
       description="A binary package with the texinfo executables">
       <HomepageHeader />
       <main>
