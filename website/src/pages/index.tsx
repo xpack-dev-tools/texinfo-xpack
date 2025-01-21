6527bacc82eaf206d80e60dea0899336abf40476
Liviu Ionescu <ilg@livius.net>
2025-01-21 15:22:58 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/texinfo@{customField('releaseVersion')} --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/texinfo@7.0.3-1.1 --verbose</InstallWithCopy>
         </div>
 
       </div>
